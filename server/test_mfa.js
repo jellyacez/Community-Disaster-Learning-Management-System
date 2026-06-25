e8066bb565df6474d9427668549926441d432490
nolifesean <deguzmanseanluigi0506@gmail.com>
2026-06-25 17:44:00 +0800

    refactor: remove test MFA script to clean up unused code

--- server/test_mfa.js
+++
@@ -1,1 +0,0 @@
-const pool = require('./config/db'); pool.query('SELECT * FROM "user" LIMIT 1').then(res => { console.log(res.rows); process.exit(0); }).catch(e => { console.error(e); process.exit(1); });
